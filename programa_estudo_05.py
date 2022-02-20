f85b0ebfd4f5869f4a8b5f70c727016d87088f56
Jose Rodrigues <99560762+Josefilhotec@users.noreply.github.com>
2022-02-19 21:06:27 -0300

    Update programa_estudo_05.py

--- programa_estudo_05.py
+++ programa_estudo_05.py
@@ -1,5 +1,6 @@
 # escreva um programa que leia a distancia de uma viajem calcule:
 # em KILOMETROS, HEQUITOMETROS, DECAMETROS, DECIMETROS, CENTIMETROS, MILIMETROS.
+print('# Exemplo 01')
 d = float(input(' Uma distancia em metros: '))  # distancia
 km = d / 1000
 hm = d / 100
@@ -15,7 +16,7 @@ print('''A medida de {} Metros, corresponde a.
 {:.0f} CENTIMETROS
 {:.0f} MILIMETROS'''.format(d, km, hm, deca, deci, cm, mm))
 
-print('# Exemplo 04')
+print('# Exemplo 02')
 d = float(input(' Uma distancia em metros: '))
 print('''A medida de {} Metros, corresponde a.
 {} KILOMETROS
